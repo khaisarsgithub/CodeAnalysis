5c9d71833104d1f8e534921fe3fbb437113f011c
khaisarsgithub <mdkhaisars118@gmail.com>
2024-10-17 10:55:08 +0530

    static files for admin panel

--- git_app/admin.py
+++ git_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Report, GitHubRepo, Project
+from .models import Report, GitHubRepo, Project, CronJob
 
 # Register your models here.
 admin.site.register(Report)
 admin.site.register(Project)
 admin.site.register(GitHubRepo)
+admin.site.register(CronJob)
